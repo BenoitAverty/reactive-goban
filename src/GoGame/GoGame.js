7f96006c2c852dcc0b3fe68df8445f061aec7297
Benoit Averty <benoit.averty@gmail.com>
2016-05-25 11:43:27 +0200

    Changed structure of the Game object and added a basic implemetation of the react component

--- src/GoGame/GoGame.js
+++ src/GoGame/GoGame.js
@@ -1,8 +1,11 @@
 function GoGame() {
-  this.stones = [];
+  this.board = {
+    4: { 4: {}, 10: {}, 16: {} },
+    10: { 4: {}, 10: {}, 16: {} },
+    16: { 4: {}, 10: {}, 16: {} },
+  };
   this.moves = [];
   this.ko = null;
-  this.marks = [];
 }
 
 GoGame.prototype = {};
--- src/ReactGoban/ReactGoban.js
+++ src/ReactGoban/ReactGoban.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
+
+const constructJsxBoard = (board) => {
+  const filledBoard = _.map(Array(19), () => _.fill(Array(19), '_'));
+
+  _.forOwn(board, (line, i) => {
+    _.forOwn(line, (column, j) => {
+      filledBoard[i-1][j-1] = 'x';
+    });
+  });
+
+  const ret = _(filledBoard)
+  .map(line => _.reduce(line, (lineBefore, el) => lineBefore + el, ''))
+  .map((line, index) => (<span key={index}>{line}<br /></span>));
+
+  return (
+    <pre>
+      {_.toArray(ret)}
+    </pre>
+  );
+};
 
 const ReactGoban = ({ game }) =>
   <div>
     <h1>Go Game:</h1>
-    <ul>
-      {game.stones.map(s => <li>`${s}`</li>)}
-    </ul>
-    <ul>
-
-    </ul>
+    {constructJsxBoard(game.board)}
   </div>
 ;
 
