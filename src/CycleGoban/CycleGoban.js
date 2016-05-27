2b4d10942b63338f89f0c66f1b0b681316fb9299
Benoit Averty <benoit.averty@gmail.com>
2016-05-27 08:22:22 +0200

    Handled actions (as sink) in CycleGoban driver to maintain a 'games' stream

--- src/CycleGoban/CycleGoban.js
+++ src/CycleGoban/CycleGoban.js
@@ -1,5 +1,24 @@
+import { div, span, makeDOMDriver } from '@cycle/dom';
 
-function makeCycleGobanDriver() {
+import actions from '../GoGame/actions';
+import goGameReducer from '../GoGame/reducer';
+
+function makeCycleGobanDriver(selectorOrElement) {
+  const domDriver = makeDOMDriver(selectorOrElement);
+
+  return (actions$) => {
+    const games$ = actions$
+      .startWith(actions.init())
+      .scan((prevGame, action) => goGameReducer(prevGame, action), undefined);
+
+    const $trees = games$.map((game) =>
+      div('.reactive-goban', [
+        span(JSON.stringify(game)),
+      ])
+    );
+
+    return domDriver($trees);
+  };
 }
 
 export default {
